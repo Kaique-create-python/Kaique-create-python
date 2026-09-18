6f673e70ed6e4d2b6e99cf80f6416006a4bae33d
Kaique pereira ramos <kp9276053@gmail.com>
2026-09-18 19:17:43 -0300

    Rename UI to v0.6 HalfDuplexFix

--- atom-studio/app/src/main/java/com/kaique/atomstudio/MainActivityV2.java
+++ atom-studio/app/src/main/java/com/kaique/atomstudio/MainActivityV2.java
@@ -91,7 +91,7 @@ public class MainActivityV2 extends Activity {
         root.setPadding(pad, pad, pad, dp(30));
         scroll.addView(root);
 
-        TextView title = text("ATOM Studio v0.5 StableAudio", 30, Color.WHITE);
+        TextView title = text("ATOM Studio v0.6 HalfDuplexFix", 30, Color.WHITE);
         title.setTypeface(Typeface.DEFAULT_BOLD);
         root.addView(title);
 
